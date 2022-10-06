4c1d06a4f9c0bbba778a5b286e4bee4a13ed8a41
Spectrum One Mac <jcdologmanding@spectrumone.co>
2022-10-06 22:25:59 +0800

    Initial commit.

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{jsx, js}'],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+  colors: {
+    blue: {
+      DEFAULT: '#2a489e'
+    },
+  }
+}
